f6ccab1e77a5e47b3f14abf962a136bcf5a0e6f2
goyalpramod <goyalpramod1729@gmail.com>
2022-12-02 20:58:18 +0530

    PUT request for car id

--- test/main.py
+++ test/main.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {"message":"Hello World"}
+class Car(BaseModel):
+    id: int
+    name: str
+    make: str
+    horsepower: int
+    color: str
 
+
+@app.get("/")
+def index():
+    return {"message": "Hello World"}
+
+
+@app.put("/car/{car_id}")
+def update_car(car_id: int, car: Car):
+    return {
+        "name": car.name,
+        "make": car.make,
+        "horsepower": car.horsepower,
+        "color": car.color,
+    }
