947fad9553050ebe456aaf00a71c1cc9e031bf9b
goyalpramod <goyalpramod1729@gmail.com>
2022-12-04 15:15:12 +0530

    update main.py

--- test/main.py
+++ test/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from database import session_local
+from typing import List
 
 app = FastAPI()
 
@@ -11,17 +13,43 @@ class Car(BaseModel):
     horsepower: int
     color: str
 
-
-@app.get("/")
-def index():
-    return {"message": "Hello World"}
+    class Config:
+        orm_mode = True
 
 
-@app.put("/car/{car_id}")
-def update_car(car_id: int, car: Car):
-    return {
-        "name": car.name,
-        "make": car.make,
-        "horsepower": car.horsepower,
-        "color": car.color,
-    }
+db = session_local()
+
+
+@app.get("/cars",response_model=List[Car],status_code=200)
+def get_all_cars():
+    pass
+
+
+@app.get("/car/{car_id}")
+def get_a_car(item_id: int):
+    pass
+
+
+@app.post("cars")
+def create_a_car():
+    pass
+
+
+@app.put("/item/{item_id}")
+def update_a_car(item_id: int):
+    pass
+
+
+@app.delete("/item/{item_id}")
+def delete_a_car(item_id: int):
+    pass
+
+
+# @app.put("/car/{car_id}")
+# def update_car(car_id: int, car: Car):
+#     return {
+#         "name": car.name,
+#         "make": car.make,
+#         "horsepower": car.horsepower,
+#         "color": car.color,
+#     }
