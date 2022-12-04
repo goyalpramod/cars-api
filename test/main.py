c928c98e6aaee5f782a47e7e3d416113e1106485
goyalpramod <goyalpramod1729@gmail.com>
2022-12-04 15:31:59 +0530

    create CRUD routes

--- test/main.py
+++ test/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from database import session_local
 from typing import List
+import models
 
 app = FastAPI()
 
@@ -20,9 +21,10 @@ class Car(BaseModel):
 db = session_local()
 
 
-@app.get("/cars",response_model=List[Car],status_code=200)
+@app.get("/cars", response_model=List[Car], status_code=200)
 def get_all_cars():
-    pass
+    cars = db.query(models.Car).all()
+    return cars
 
 
 @app.get("/car/{car_id}")
@@ -30,9 +32,20 @@ def get_a_car(item_id: int):
     pass
 
 
-@app.post("cars")
-def create_a_car():
-    pass
+@app.post("/cars", response_model=Car, status_code=201)
+def create_a_car(car:Car):
+    new_car=models.Car(
+        id=car.id,
+        name=car.name,
+        make=car.make,
+        horsepower=car.horsepower,
+        color=car.color
+    )
+
+    db.add(new_car)
+    db.commit()
+
+    return new_car
 
 
 @app.put("/item/{item_id}")
